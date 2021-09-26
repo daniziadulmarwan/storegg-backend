912d017d4e6e5b29a469bd8f9606a03733b035b3
daniziadulmarwan <zeronosvegaaltair@gmail.com>
2021-09-26 13:26:31 +0700

    add

--- app/category/controller.js
+++ app/category/controller.js
@@ -1,7 +1,7 @@
 class Category {
   async index(req, res) {
     try {
-      res.render("index", { title: "StoreGG" });
+      res.render("admin/category/view_category", { title: "StoreGG" });
     } catch (error) {
       console.log(error);
     }
