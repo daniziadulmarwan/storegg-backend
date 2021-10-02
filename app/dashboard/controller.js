759f77ebd803dc2904c85a2c66ee27e0ebef919c
daniziadulmarwan <zeronosvegaaltair@gmail.com>
2021-10-02 18:23:55 +0700

    add

--- app/dashboard/controller.js
+++ app/dashboard/controller.js
@@ -1,7 +1,20 @@
+const Transaction = require("../transaction/model");
+const Voucher = require("../voucher/model");
+const Category = require("../category/model");
+
 module.exports = {
   index: async (req, res) => {
     try {
-      res.render("index", { title: "Dashboard", user: req.session.user });
+      let transaction = await Transaction.countDocuments();
+      let voucher = await Voucher.countDocuments();
+      let category = await Category.countDocuments();
+      res.render("admin/dashboard/index", {
+        title: "Dashboard",
+        user: req.session.user,
+        transaction,
+        voucher,
+        category,
+      });
     } catch (error) {
       console.log(error);
     }
