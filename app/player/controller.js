f72ca6e974c1b1227b45c51344cf839cdf7ebd32
daniziadulmarwan <zeronosvegaaltair@gmail.com>
2021-10-02 21:38:51 +0700

    add

---
+++ app/player/controller.js
@@ -0,0 +1,17 @@
+const Player = require("./model");
+const Voucher = require("../voucher/model");
+
+module.exports = {
+  landingPage: async (req, res) => {
+    try {
+      let voucher = await Voucher.find()
+        .select("id name status category thumbnail")
+        .populate("category");
+      res.status(200).json({ data: voucher });
+    } catch (error) {
+      res.status(500).json({
+        message: error.message || "Terjadi kesalahan pada server",
+      });
+    }
+  },
+};
