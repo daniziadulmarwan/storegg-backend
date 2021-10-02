b13044fe551de71ca7c5ce042f19b55df3ced05f
daniziadulmarwan <zeronosvegaaltair@gmail.com>
2021-10-02 16:44:44 +0700

    add

---
+++ app/transaction/controller.js
@@ -0,0 +1,38 @@
+const Transaction = require("./model");
+
+module.exports = {
+  index: async (req, res) => {
+    try {
+      const alertMessage = req.flash("alertMessage");
+      const alertStatus = req.flash("alertStatus");
+      const alert = { status: alertStatus, message: alertMessage };
+      const transactions = await Transaction.find();
+      res.render("admin/transaction/index", {
+        title: "Transaction",
+        transactions,
+        alert,
+        user: req.session.user,
+      });
+    } catch (error) {
+      req.flash("alertMessage", `${error.message}`);
+      req.flash("alertStatus", "danger");
+      res.redirect("/transaction");
+    }
+  },
+
+  actionStatus: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const { status } = req.query;
+
+      await Transaction.findByIdAndUpdate(id, { status });
+      req.flash("alertMessage", `Sukses ubah status`);
+      req.flash("alertStatus", "success");
+      res.redirect("/transaction");
+    } catch (error) {
+      req.flash("alertMessage", `${error.message}`);
+      req.flash("alertStatus", "danger");
+      res.redirect("/transaction");
+    }
+  },
+};
